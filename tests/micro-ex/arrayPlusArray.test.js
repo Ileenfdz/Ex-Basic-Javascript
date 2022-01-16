3bdc56b0a4e2287e6493dec4ed4d5eb14ec93cef
Ileenfdz <ileenfdz@gmail.com>
2022-01-16 17:40:45 +0100

    Refactored arrayPlusArray.test

--- tests/micro-ex/arrayPlusArray.test.js
+++ tests/micro-ex/arrayPlusArray.test.js
@@ -6,23 +6,14 @@ describe('Fundamentals level 1 - Array Plus Array', () => {
     });
 
     test('should return -21', () => {
-        let firstArr = [-1, -2, -3];
-        let secondArr = [-4, -5, -6];
-        let result = arrSum(firstArr, secondArr);
-        expect(result).toEqual(-21);
+        expect(arrSum([-1, -2, -3], [-4, -5, -6])).toEqual(-21);
     });
 
     test('should return 15', () => {
-        let firstArr = [0, 0, 0];
-        let secondArr = [4, 5, 6];
-        let result = arrSum(firstArr, secondArr);
-        expect(result).toEqual(15);
+        expect(arrSum([0, 0, 0], [4, 5, 6])).toEqual(15);
     });
 
     test('should return 2100', () => {
-        let firstArr = [100, 200, 300];
-        let secondArr = [400, 500, 600];
-        let result = arrSum(firstArr, secondArr);
-        expect(result).toEqual(2100);
+        expect(arrSum([100, 200, 300], [400, 500, 600])).toEqual(2100);
     });
 });
